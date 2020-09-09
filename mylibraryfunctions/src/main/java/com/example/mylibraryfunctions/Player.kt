857acb651742157281bffd8c5205c445b4251c54
Gilbert <gilbert@apppay.tech>
2020-09-09 09:24:55 +0800

    activate library

---
+++ mylibraryfunctions/src/main/java/com/example/mylibraryfunctions/Player.kt
@@ -0,0 +1,12 @@
+package com.example.mylibraryfunctions
+
+class Player {
+    
+    lateinit var name: String
+    lateinit var role: String
+    lateinit var age: String
+
+    fun add(add:Int,add2:Int):Int{
+        return (add+add2)
+    }
+}
